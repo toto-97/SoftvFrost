847f12853e26a1eb8b8b823435997b5baea6e96f
isfon <lamgers@gmail.com>
2017-02-20 23:41:32 -0600

    se agrego ui ruter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,35 @@
  *
  * Main module of the application.
  */
-angular
-  .module('softvFrostApp', [
-    'ngAnimate',
-    'ngSanitize'
-  ]);
+angular.module('softvFrostApp', [
+		'ngAnimate',
+		'ngSanitize',
+		'ui.router'
+	])
+	.config(['$provide', '$urlRouterProvider', '$httpProvider', '$qProvider', function($provide, $urlRouterProvider, $httpProvider, $qProvider) {
+		$urlRouterProvider.otherwise('/auth/login');
+		$provide.factory('ErrorHttpInterceptor', function($q, $injector) {
+			function notifyError(rejection) {
+				console.log(rejection);
+			}
+			return {
+				requestError: function(rejection) {
+					notifyError(rejection);
+					return $q.reject(rejection);
+				},
+				responseError: function(rejection) {
+					notifyError(rejection);
+					sessionStorage.clear();
+					return $q.reject(rejection);
+				}
+			};
+		});
+		$httpProvider.interceptors.push('ErrorHttpInterceptor');
+		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	}])
+	.constant('APP_CONFIG', window.appConfig)
+	.run(['$rootScope', '$state', '$stateParams', '$location', function($rootScope, $state, $stateParams, $location) {
+		$rootScope.$state = $state;
+		$rootScope.$stateParams = $stateParams;
+	}]);
