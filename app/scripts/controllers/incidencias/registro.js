dbdaf6dc8091bdad2845cde3b3a2a2d0e122dc0d
Eduardo González <laloe.90@gmail.com>
2017-02-22 19:53:46 -0600

    trabajando con servicio añadir ticket

--- app/scripts/controllers/incidencias/registro.js
+++ app/scripts/controllers/incidencias/registro.js
@@ -1,6 +1,26 @@
 'use strict';
-angular.module('softvFrostApp').controller('RegistroCtrl', RegistroCtrl);
 
-function RegistroCtrl() {
-	this.hola = 'adsasdas';
+function RegistroCtrl(ngNotify, incidenciasFactory, $state) {
+	function guardar() {
+		var addTi = {
+			ticketPadre: 0,
+			san: 1,
+			fecha: vm.fecha,
+			sintoma: vm.sintoma,
+			prioridad: vm.prioridad,
+			descripcion: vm.descripcion
+		};
+		incidenciasFactory.addSuscriptor(addTi).then(function(data) {
+			if (data.AddTicketResult > 0) {
+				ngNotify.set('Suscriptor agregado correctamente.', 'success');
+				$state.go('home.incidiencias.registro');
+			} else {
+				ngNotify.set('Error al agregar el suscriptor.', 'error');
+			}
+		});
+	}
+
+	var vm = this;
+	vm.guardar = guardar;
 }
+angular.module('softvFrostApp').controller('RegistroCtrl', RegistroCtrl);
---
+++ app/scripts/factories/incidenciasFactory.js
@@ -0,0 +1,65 @@
+'use strict';
+angular.module('softvFrostApp')
+	.factory('incidenciasFactory', function($http, $q, $window, globalService, $localStorage) {
+		var factory = {};
+		var paths = {
+			addTicket: '/Ticket/AddTicket',
+			updateTicket: '/Ticket/UpdateTicket'
+		};
+		factory.addTicket = function(obj) {
+			var deferred = $q.defer();
+			var Parametros = {
+                "objTicket":
+                    {
+                        "IdUsuario": $localStorage.currentUser.idUsuario,
+                        "IdTicketPadre": 0,
+                        "SAN": 1,
+                        "Fecha": obj.fecha,
+                        "Sintoma": obj.sintoma,
+                        "Prioridad": obj.prioridad,
+                        "Descripcion": obj.descripcion
+                    }
+			};
+			var config = {
+				headers: {
+					'Authorization': $localStorage.currentUser.token
+				}
+			};
+			$http.post(globalService.getUrl() + paths.addTicket, JSON.stringify(Parametros), config).then(function(response) {
+
+			}).catch(function(data) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
+		}
+
+		factory.updateTicket = function() {
+			var deferred = $q.defer();
+			var Parametros = {
+                "objTicket":
+                    {
+						"IdTicket":2,
+                        "IdUsuario": $localStorage.currentUser.idUsuario,
+                        "IdTicketPadre": 0,
+                        "SAN": 1,
+                        "Fecha": "22/01/2017",
+                        "Sintoma": "aaaa",
+                        "Prioridad": "aaa",
+                        "Descripcion": "aaaaaaaaaa"
+                    }
+			};
+			var config = {
+				headers: {
+					'Authorization': $localStorage.currentUser.token
+				}
+			};
+			$http.post(globalService.getUrl() + paths.updateTicket, JSON.stringify(Parametros), config).then(function(response) {
+
+			}).catch(function(data) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
+		}
+
+		return factory;
+	});
