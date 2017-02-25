dbafd5430ed670d05fdf7b27bde7049badc7921c
Eduardo González <laloe.90@gmail.com>
2017-02-24 20:02:57 -0600

    agregar ticket

--- app/scripts/controllers/incidencias/registro.js
+++ app/scripts/controllers/incidencias/registro.js
@@ -1,17 +1,58 @@
 'use strict';
 
-function RegistroCtrl(ngNotify, incidenciasFactory, $state) {
+function RegistroCtrl(ngNotify, incidenciasFactory, $state, $filter) {
+	function initial() {
+		incidenciasFactory.getMotivo().then(function(data) {
+			data.GetMotivoTicketListResult.unshift({
+				'Descripcion': 'Seleccione motivo',
+				'IdMotivoTicket': 0
+			});
+			vm.motivo = data.GetMotivoTicketListResult;
+			vm.selectedMotivo = vm.motivo[0];
+		});
+		incidenciasFactory.getSintoma().then(function(data) {
+			data.GetSintomaListResult.unshift({
+				'Descripcion': 'Seleccione síntoma',
+				'IdSintoma': 0
+			});
+			vm.sintoma = data.GetSintomaListResult;
+			vm.selectedSintoma = vm.sintoma[0];
+		});
+		incidenciasFactory.getTipoContrato().then(function(data) {
+			data.GetTipoContactoListResult.unshift({
+				'Nombre': 'Seleccione tipo contacto',
+				'IdTipoContacto': 0
+			});
+			vm.tipoContacto = data.GetTipoContactoListResult;
+			vm.selectedTipoContacto = vm.tipoContacto[0];
+		});
+		incidenciasFactory.getMedio().then(function(data) {
+			data.GetMedioComunicacionListResult.unshift({
+				'Nombre': 'Seleccione medio de comunicación',
+				'IdMedioComunicacion': 0
+			});
+			vm.medioComun = data.GetMedioComunicacionListResult;
+			vm.selectedMedioComun = vm.medioComun[0];
+		});
+	}
+
 	function guardar() {
+		vm.auxFecha = $filter('date')(vm.fechaRegistro, 'dd/MM/yyyy');
 		var addTi = {
 			san: vm.san,
-			fecha: vm.fecha,
+			fecha: vm.auxFecha,
 			sintoma: vm.sintoma,
+			motivo: vm.selectedMotivo.IdMotivoTicket,
 			prioridad: vm.prioridad,
 			descripcion: vm.descripcion,
-			nombreContrato: vm.nombreContrato,
-			numeroContrato: vm.numeroContrato
+			tipoContacto: vm.selectedTipoContacto.IdTipoContacto,
+			nombreContacto: vm.nombreContacto,
+			medioComun: vm.selectedMedioComun.IdMedioComunicacion,
+			numeroContacto: vm.numeroContacto
 		};
 		console.log(addTi);
+		console.log(vm.selectedMotivo.IdMotivoTicket);
+
 		// incidenciasFactory.addSuscriptor(addTi).then(function(data) {
 		// 	if (data.AddTicketResult > 0) {
 		// 		ngNotify.set('Suscriptor agregado correctamente.', 'success');
@@ -23,24 +64,37 @@ function RegistroCtrl(ngNotify, incidenciasFactory, $state) {
 	}
 
 	function getTerminal() {
-		console.log(vm.terminal);
-		incidenciasFactory.getTerminal(vm.terminal).then(function(data) {
-			vm.terminalDatos = data.GetByTerminalResult;
-			console.log(vm.terminalDatos);
-		});
+		if (vm.san == undefined) {
+			ngNotify.set('Inserte número de terminal.', 'error');
+		}else {
+			vm.busqueda = true;
+			incidenciasFactory.getTerminal(vm.san).then(function(data) {
+				vm.terminalDatos = data.GetByTerminalResult;
+				console.log(vm.terminalDatos);
+				console.log(data);
+			});
+		}
 	}
 
 	function limpiar() {
-		vm.terminal = '';
+		vm.san = '';
 		vm.motivo = '';
 		vm.sintoma = '';
 		vm.tipoContacto = '';
 		vm.medioComun = '';
+		vm.prioridad = '';
+		vm.nombreContacto = '';
+		vm.numeroContacto = '';
+		vm.descripcion = '';
+		vm.busqueda = false;
+		initial();
 	}
 
 	var vm = this;
 	vm.guardar = guardar;
 	vm.limpiar = limpiar;
 	vm.getTerminal = getTerminal;
+	vm.fechaRegistro = new Date();
+	initial();
 }
 angular.module('softvFrostApp').controller('RegistroCtrl', RegistroCtrl);
--- app/scripts/factories/incidenciasFactory.js
+++ app/scripts/factories/incidenciasFactory.js
@@ -4,8 +4,12 @@ angular.module('softvFrostApp')
 		var factory = {};
 		var paths = {
 			addTicket: '/Ticket/AddTicket',
-			getTickets: 'Ticket/GetTicketList',
-			getTerminal: '/Terminal/GetByTerminal'
+			getTickets: '/Ticket/GetTicketList',
+			getTerminal: '/Terminal/GetByTerminal',
+			getMotivo: '/MotivoTicket/GetMotivoTicketList',
+			getSintoma: '/Sintoma/GetSintomaList',
+			getTipoContrato: '/TipoContacto/GetTipoContactoList',
+			getMedio: '/MedioComunicacion/GetMedioComunicacionList'
 		};
 		factory.addTicket = function(obj) {
 			var deferred = $q.defer();
@@ -16,13 +20,16 @@ angular.module('softvFrostApp')
                         "IdTicketPadre": 0,
                         "SAN": obj.san,
                         "Fecha": obj.fecha,
-                        "Sintoma": obj.sintoma,
-                        "Prioridad": obj.prioridad,
+                        "IdSintoma": obj.sintoma,
+                        "IdMotivoTicket": obj.motivo,
+						"Prioridad": obj.prioridad,
                         "Descripcion": obj.descripcion,
-						"IdTipoContacto":1,
+						"IdTipoContacto":obj.tipoContacto,
 						"NombreContacto": obj.nombreContrato,
-						"IdMedioComunicacion":1,
-						"NumeroContacto": obj.numeroContrato
+						"IdMedioComunicacion":obj.medioComun,
+						"NumeroContacto": obj.numeroContrato,
+						"FechaCierre": "",
+						"Estado": "P"
                     }
 			};
 			var config = {
@@ -31,7 +38,7 @@ angular.module('softvFrostApp')
 				}
 			};
 			$http.post(globalService.getUrl() + paths.addTicket, JSON.stringify(Parametros), config).then(function(response) {
-
+				deferred.resolve(response.data);
 			}).catch(function(data) {
 				deferred.reject(data);
 			});
@@ -49,44 +56,17 @@ angular.module('softvFrostApp')
 				}
 			};
 			$http.post(globalService.getUrl() + paths.getTickets, JSON.stringify(), config).then(function(response) {
-
+				deferred.resolve(response.data);
 			}).catch(function(data) {
 				deferred.reject(data);
 			});
 			return deferred.promise;
 		}
 
-		factory.updateTicket = function() {
+		factory.getTerminal = function(san) {
 			var deferred = $q.defer();
 			var Parametros = {
-                "objTicket":
-                    {
-                        "IdUsuario": $localStorage.currentUser.idUsuario,
-                        "IdTicketPadre": 0,
-                        "SAN": obj.san,
-                        "Fecha": obj.fecha,
-                        "Sintoma": "aaaa",
-                        "Prioridad": "aaa",
-                        "Descripcion": "aaaaaaaaaa"
-                    }
-			};
-			var config = {
-				headers: {
-					'Authorization': $localStorage.currentUser.token
-				}
-			};
-			$http.post(globalService.getUrl() + paths.updateTicket, JSON.stringify(Parametros), config).then(function(response) {
-
-			}).catch(function(data) {
-				deferred.reject(data);
-			});
-			return deferred.promise;
-		}
-
-		factory.getTerminal = function(terminal) {
-			var deferred = $q.defer();
-			var Parametros = {
-				  "SAN": terminal
+				  "SAN": san
 				};
 			var config = {
 				headers: {
@@ -94,7 +74,75 @@ angular.module('softvFrostApp')
 				}
 			};
 			$http.post(globalService.getUrl() + paths.getTerminal, JSON.stringify(Parametros), config).then(function(response) {
+				deferred.resolve(response.data);
+			}).catch(function(data) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
+		}
 
+		factory.getMotivo = function(san) {
+			var deferred = $q.defer();
+			var Parametros = {
+				};
+			var config = {
+				headers: {
+					'Authorization': $localStorage.currentUser.token
+				}
+			};
+			$http.get(globalService.getUrl() + paths.getMotivo, config).then(function(response) {
+				deferred.resolve(response.data);
+			}).catch(function(data) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
+		}
+
+		factory.getSintoma = function(san) {
+			var deferred = $q.defer();
+			var Parametros = {
+				};
+			var config = {
+				headers: {
+					'Authorization': $localStorage.currentUser.token
+				}
+			};
+			$http.get(globalService.getUrl() + paths.getSintoma, config).then(function(response) {
+				deferred.resolve(response.data);
+			}).catch(function(data) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
+		}
+
+		factory.getTipoContrato = function(san) {
+			var deferred = $q.defer();
+			var Parametros = {
+				};
+			var config = {
+				headers: {
+					'Authorization': $localStorage.currentUser.token
+				}
+			};
+			$http.get(globalService.getUrl() + paths.getTipoContrato, config).then(function(response) {
+				deferred.resolve(response.data);
+			}).catch(function(data) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
+		}
+
+		factory.getMedio = function(san) {
+			var deferred = $q.defer();
+			var Parametros = {
+				};
+			var config = {
+				headers: {
+					'Authorization': $localStorage.currentUser.token
+				}
+			};
+			$http.get(globalService.getUrl() + paths.getMedio, config).then(function(response) {
+				deferred.resolve(response.data);
 			}).catch(function(data) {
 				deferred.reject(data);
 			});
