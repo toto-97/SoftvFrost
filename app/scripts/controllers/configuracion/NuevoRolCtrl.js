fe806979a1a8706c390aa31bcea262172d359012
Unknown <erictics@gmail.com>
2017-02-26 02:25:13 -0600

    guardar roles
    falta validar

---
+++ app/scripts/controllers/configuracion/NuevoRolCtrl.js
@@ -0,0 +1,24 @@
+'use strict';
+angular.module('softvFrostApp').controller('NuevoRolCtrl', NuevoRolCtrl);
+
+function NuevoRolCtrl(usuarioFactory, rolFactory, $state, ngNotify) {
+
+  function init() {
+
+  };
+
+  function GuardarRol() {
+    var obj = {};
+    obj.Nombre = vm.Nombre;
+    obj.Descripcion = vm.Descripcion;
+    obj.Estado = vm.Estatus;
+    rolFactory.AddRole(obj).then(function(data) {
+      $state.go('home.provision.roles');
+      ngNotify.set('Rol agregado correctamente.', 'success');
+    });
+  }
+  var vm = this;
+  init();
+  vm.Estatus = false;
+  vm.GuardarRol = GuardarRol;
+}
--- app/scripts/factories/rolFactory.js
+++ app/scripts/factories/rolFactory.js
@@ -1,27 +1,52 @@
 'use strict';
 angular.module('softvFrostApp')
-	.factory('rolFactory', function($http, $q, $window, globalService, $localStorage) {
-		var factory = {};
-		var paths = {
-			GetRoleList: '/Role/GetRoleList',
-			AddRole: '/Role/AddRole',
-			UpdateRole: '/Role/UpdateRole',
-		};
+  .factory('rolFactory', function($http, $q, $window, globalService, $localStorage) {
+    var factory = {};
+    var paths = {
+      GetRoleList: '/Role/GetRoleList',
+      AddRole: '/Role/AddRole',
+      UpdateRole: '/Role/UpdateRole',
+    };
 
-		factory.GetRoleList=function(){
-			var deferred = $q.defer();
-			var config = {
-				headers: {
-					'Authorization': $localStorage.currentUser.token
-				}
-			};
-			$http.get(globalService.getUrl() + paths.GetRoleList, config).then(function(response) {
-				deferred.resolve(response.data);
-			}).catch(function(data) {
-				deferred.reject(data);
-			});
-			return deferred.promise;
-		}
+    factory.GetRoleList = function() {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      $http.get(globalService.getUrl() + paths.GetRoleList, config).then(function(response) {
+        deferred.resolve(response.data);
+      }).catch(function(data) {
+        deferred.reject(data);
+      });
+      return deferred.promise;
+    }
 
-		return factory;
+
+    factory.AddRole = function(object) {
+      var deferred = $q.defer();
+      var Parametros = {
+        'objRole': {
+          'Nombre': object.Nombre,
+          'Descripcion': object.Descripcion,
+          "Estado": object.Estado
+
+        }
+      };
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      $http.post(globalService.getUrl() + paths.AddRole, JSON.stringify(Parametros), config).then(function(response) {
+        deferred.resolve(response.data);
+      }).catch(function(data) {
+        deferred.reject(data);
+      });
+      return deferred.promise;
+
+    }
+
+    return factory;
   });
