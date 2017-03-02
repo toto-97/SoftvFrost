e3304ca8efb41a44104d8a2d0b8b87ba9929be1e
Unknown <erictics@gmail.com>
2017-03-02 00:19:32 -0600

    inicio de pantalla de permisos

--- app/scripts/controllers/configuracion/PermisosCtrl.js
+++ app/scripts/controllers/configuracion/PermisosCtrl.js
@@ -1,6 +1,49 @@
 'use strict';
 angular.module('softvFrostApp').controller('PermisosCtrl', PermisosCtrl);
 
-function PermisosCtrl(permisoFactory){
-  var vm=this;
+function PermisosCtrl(permisoFactory, rolFactory) {
+  function Init() {
+    rolFactory.GetRoleList().then(function(data) {
+      vm.Roles = data.GetRoleListResult;
+      vm.Rol = vm.Roles[0];
+      GetModuleList();
+    });
+  }
+
+  function GetModuleList() {
+    permisoFactory.GetModuleList().then(function(data) {
+      var modulos = data.GetModuleListResult;
+      console.log(data);
+      permisoFactory.GetPermisoList(vm.Rol.IdRol).then(function(data) {
+        var permisos = data.GetPermisoRolListResult;
+        console.log(data.GetPermisoRolListResult);
+        vm.Modules = MergePermisos(modulos, permisos);
+      });
+    });
+  }
+
+  function MergePermisos(modulos, permisos) {
+    for (var a = 0; a < modulos.length; a++) {
+      for (var b = 0; b < permisos.length; b++) {
+        if (modulos[a].IdModule == permisos[b].IdModule) {
+          modulos[a].OptAdd = permisos[b].OptAdd;
+          modulos[a].OptDelete = permisos[b].OptDelete;
+          modulos[a].OptUpdate = permisos[b].OptUpdate;
+          modulos[a].OptSelect = permisos[b].OptSelect;
+
+        }
+      }
+
+    }
+    return modulos;
+  }
+
+  function ObtenPermisos() {
+    GetModuleList();
+  }
+
+
+  var vm = this;
+  Init();
+  vm.ObtenPermisos = ObtenPermisos;
 }
--- app/scripts/factories/permisoFactory.js
+++ app/scripts/factories/permisoFactory.js
@@ -1,5 +1,46 @@
 'use strict';
 angular.module('softvFrostApp')
-	.factory('permisoFactory', function($http, $q, $window, globalService, $localStorage) {
+  .factory('permisoFactory', function($http, $q, $window, globalService, $localStorage) {
+    var factory = {};
+    var paths = {
+      GetModuleList: '/Module/GetModuleList',
+      GetPermisoList: '/Permiso/GetPermisoRolList',
+    };
+    factory.GetModuleList = function() {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      $http.get(globalService.getUrl() + paths.GetModuleList, config).then(function(response) {
+        deferred.resolve(response.data);
+      }).catch(function(data) {
+        deferred.reject(data);
+      });
+      return deferred.promise;
+    }
+
+
+    factory.GetPermisoList = function(idrol) {
+      var deferred = $q.defer();
+      var Parametros = {
+        'IdRol': idrol
+      };
+
+      console.log(Parametros);
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      $http.get(globalService.getUrl() + paths.GetPermisoList, JSON.stringify(Parametros), config).then(function(response) {
+        deferred.resolve(response.data);
+      }).catch(function(data) {
+        deferred.reject(data);
+      });
+      return deferred.promise;
+    }
+    return factory;
 
   });
