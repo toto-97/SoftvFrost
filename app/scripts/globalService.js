f11a1d1dac1ad6df43811e48ebc2bed4b857f247
isfon <lamgers@gmail.com>
2017-04-17 09:54:10 -0500

    cambios

--- app/scripts/globalService.js
+++ app/scripts/globalService.js
@@ -7,7 +7,6 @@ angular.module('softvFrostApp')
 			return 'http://35.164.143.107/SoftvWCFService.svc';
 		};
 		svc.getUrlHughesService = function() {
-			//return 'http://35.164.143.107:8081/SoftvFrostAPI';
 			return 'http://192.168.50.20:5000/api';
 		};
 
