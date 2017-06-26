6870b4f08217c50621f3958fd70489275d3f72b6
EricChavez <erictics@gmail.com>
2017-06-26 17:30:52 -0500

    MAPA DE TERMINALES

--- app/scripts/globalService.js
+++ app/scripts/globalService.js
@@ -11,11 +11,11 @@ angular.module('softvFrostApp')
 		};
 
 		svc.getUrlBeams = function () {
-			return 'http://192.168.50.33:3000/BossMonitoreo/beams/';
+			return 'http://189.254.231.35/beams/';
 		};
 
 		svc.getUrlHughesMonitoreo = function () {
-			return 'http://192.168.50.33:3000/BossMonitoreo';
+			return 'http://192.168.50.33:3000/BossMonitoreo/api';
 		};
 
 		return svc;
