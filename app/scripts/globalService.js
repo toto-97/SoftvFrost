9abdccdbb16c8214e2fe5fb15e7695e2501a09de
EricChavez <erictics@gmail.com>
2017-07-08 11:57:31 -0500

    Cambio global

--- app/scripts/globalService.js
+++ app/scripts/globalService.js
@@ -3,7 +3,7 @@ angular.module('softvFrostApp')
   .service('globalService', function () {
     var svc = {};
 //rutas servidor producción
-    /*svc.getUrl = function() {
+   svc.getUrl = function() {
     	return 'http://189.254.231.35:9091/SoftvWCFService.svc';
     	
     	
@@ -12,36 +12,18 @@ angular.module('softvFrostApp')
     	return 'http://189.254.231.35:9092/SoftvFrostAPI';				
     };
 
-		svc.getUrl = function() {
-<<<<<<< HEAD
-			//return 'http://192.168.50.33:3000/SoftvWCFService.svc';
-			return 'http://189.254.231.35:9091/SoftvWCFService.svc';
-=======
-			 	return 'http://localhost:64481/SoftvWCFService.svc';
-			//return 'http://189.254.231.35:9091/SoftvWCFService.svc';
-			
->>>>>>> develop
-		};
-		svc.getUrlHughesService = function () {
-			// 	return 'http://35.164.143.107:8081/SoftvFrostAPI';
-			return 'http://189.254.231.35:9092/SoftvFrostAPI';
-				
-		};
+    svc.getUrlBeams = function () {			
+    	return 'http://189.254.231.35/beams/';			
+    };
 
-		svc.getUrlBeams = function () {
-<<<<<<< HEAD
-			//return 'http://189.254.231.35/beams/';
-			return 'http://189.254.231.35/beams/';
-=======
-			// 	return 'http://35.164.143.107:8050/beams/';
-			return 'http://189.254.231.35/beams/';
-			
->>>>>>> develop
-		};
+    svc.getUrlHughesMonitoreo = function () {				
+    	return 'http://189.254.231.35:9090/api';
+    		
+    };
 
 
 //rutas locales
-    svc.getUrl = function () {
+ /* svc.getUrl = function () {
       return 'http://192.168.50.33:3000/SoftvWCFService.svc';
 
 
@@ -55,9 +37,9 @@ angular.module('softvFrostApp')
     };
 
     svc.getUrlHughesMonitoreo = function () {
-      return 'http://192.168.50.33:3000/api';
+      return 'http://192.168.50.33:3000/BossMonitoreo/api';
 
-    };
+    };*/
 
     return svc;
   });
